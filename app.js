894c6b7e783024322146bf3c5c29a8cedbc15fa8
Mert <nagodretrem@gmail.com>
2023-01-03 21:36:57 +0300

    Add static files

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const port = 3000;
 
+// Set view engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
+// Routes
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 app.listen(port, () => {
