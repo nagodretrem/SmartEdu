0004a107d2285b6a7c40a606777435c75bcdb6d7
Mert <nagodretrem@gmail.com>
2023-01-08 16:48:43 +0300

    Add login page

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getAboutPage,
   getIndexPage,
+  getLoginPage,
   getRegisterPage,
 } from "../controllers/pageController.js";
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.route("/").get(getIndexPage);
 router.route("/about").get(getAboutPage);
 router.route("/register").get(getRegisterPage);
+router.route("/login").get(getLoginPage);
 
 export default router;
